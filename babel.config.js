82de9666259da39d66c4009a014dd3dc5101073b
Dan Robinson <danrobinson@gmail.com>
2022-08-22 12:53:04 +0100

    Remove babel

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  sourceType: 'unambiguous',
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }]
-  ],
-  plugins: [
-    ['@babel/transform-runtime']
-  ]
-}
